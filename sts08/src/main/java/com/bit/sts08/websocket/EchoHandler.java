52f6cf61aeffc4bdbf018405073b76b77db6abba
bit01class <bit01class@gmail.com>
2021-03-29 16:16:02 +0900

    Spring Websocket

--- sts08/src/main/java/com/bit/sts08/websocket/EchoHandler.java
+++ sts08/src/main/java/com/bit/sts08/websocket/EchoHandler.java
@@ -1,25 +1,41 @@
 package com.bit.sts08.websocket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class EchoHandler extends TextWebSocketHandler {
+	List<WebSocketSession> list=new ArrayList<>();
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("Connected...");
+		System.out.println(session.getId());
+		System.out.println(session.getAttributes());
+		list.add(session);
 	}
 	
 	@Override
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
 		Object obj = message.getPayload();
 		System.out.println(obj);
+		for(WebSocketSession sockSession:list)
+				sockSession.sendMessage(message);
 	}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		list.remove(session);
 		System.out.println("close...");
 	}
 }
+
+
+
+
+
+
+
--- sts08/src/main/webapp/WEB-INF/views/home.jsp
+++ sts08/src/main/webapp/WEB-INF/views/home.jsp
@@ -6,14 +6,32 @@
 	<title>Home</title>
 	<meta charset="utf-8">
 	<script type="text/javascript" src="webjars/jquery/3.6.0/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.1/sockjs.min.js" integrity="sha512-hsqWiVBsPC5Hz9/hy5uQX6W6rEtBjtI8vyOAR4LAFpZAbQjJ43uIdmKsA7baQjM318sf8BBqrMkcWsfSsaWCNg==" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="webjars/sockjs-client/1.5.0/dist/sockjs.min.js"></script>
+	<script type="text/javascript">
+	var sock;
+	function callback(e){
+		console.log('>>>>',e.data);
+		$('<li>'+e.data+'</li>').appendTo('ul');
+	}
+	$(function(){
+		sock=new SockJS('/sts08/echo');
+		sock.onmessage=callback;
+		$('button').click(function(){
+			var msg=$('input').val();
+			sock.send(msg);
+			$('input').val("");
+		});
+	});
 	
+	</script>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+<h1>웹채팅</h1>
+<div>
+	<input/><button>전송</button>
+</div>
+<div>
+	<ul></ul>
+</div>
 </body>
 </html>
